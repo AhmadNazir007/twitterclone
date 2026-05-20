d0d81628d2e0f91138973f91feca18de8cd621c1
AhmadNazir007 <ahmadnazir866@gmail.com>
2026-05-20 12:41:12 +0500

    fix: disable frontend Socket.IO NEXT_PUBLIC_ENABLE_SOCKET=false

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -6,25 +6,28 @@ import { toast } from 'react-toastify';
 export const useSocket = () => {
   useEffect(() => {
     if (!process.env.NEXT_PUBLIC_API_URL) return;
+    if (process.env.NEXT_PUBLIC_ENABLE_SOCKET?.toLowerCase() === 'false') {
+      console.log('Socket.IO disabled by NEXT_PUBLIC_ENABLE_SOCKET=false');
+      return;
+    }
 
-    const socket = io(process.env.NEXT_PUBLIC_API_URL); // backend URL
+    const socket = io(process.env.NEXT_PUBLIC_API_URL, {
+      path: '/socket.io',
+      transports: ['websocket', 'polling'],
+    });
 
     socket.on('connect', () => {
       console.log('Connected to WebSocket');
     });
 
     type NotificationData = {
-			message: string;
-			[key: string]: unknown;
-		};
-
-		socket.on('notification', (data: NotificationData) => {
-			console.log('Notification Alert:', data);
-			toast.info(data.message);
-		});
-
-    return () => {
-      socket.disconnect();
+      message: string;
+      [key: string]: unknown;
     };
+
+    socket.on('notification', (data: NotificationData) => {
+      console.log('Notification Alert:', data);
+      toast.info(data.message);
+    });
   }, []);
 };
